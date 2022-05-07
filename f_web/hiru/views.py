09eef471de1a27738c3edd7d70c9b07eaae644d8
Na-sanghwa <8008165@gmail.com>
2022-05-07 22:45:43 +0900

    fixed the typo lines 8 '-create_data' to '-create_date'

--- f_web/hiru/views.py
+++ f_web/hiru/views.py
@@ -5,6 +5,6 @@ from django.shortcuts import render
 from .models import Question
 
 def index(request):
-    question_list=Question.objects.order_by('-create_data') #질문 목록 데이터 역순으로 정렬
+    question_list=Question.objects.order_by('-create_date') ##질문 목록 데이터 역순으로 정렬
     context={'question_list': question_list}
-    return render(request,'hiru/question_list.html',context)#템플릿에 적용해 html로 반환
+    return render(request,'hiru/question_list.html',context)##템플릿에 적용해 html로 반환
