1c95cf1c98dfb0f7d637e8dbc84dfe23f4808284
Na-sanghwa <8008165@gmail.com>
2022-05-18 09:46:36 +0900

    Update all

--- f_web/hiru/urls.py
+++ f_web/hiru/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),#별칭 사용
+    path('<int:question_id>/', views.detail, name='detail'), #question_id int 매핑, 뷰 호출, 별칭 매핑
 ]
--- f_web/hiru/views.py
+++ f_web/hiru/views.py
@@ -1,10 +1,15 @@
 
 
 # Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Question
 
 def index(request):
     question_list=Question.objects.order_by('-create_date') ##질문 목록 데이터 역순으로 정렬
     context={'question_list': question_list}
     return render(request,'hiru/question_list.html',context)##템플릿에 적용해 html로 반환
+
+def detail(request, question_id): #uri 매핑 규칙에 의해 실행
+    question = get_object_or_404(Question, pk=question_id) # 없는 페이지 조회시 404 출력
+    context = {'question': question}
+    return render(request, 'hiru/question_detail.html', context)
