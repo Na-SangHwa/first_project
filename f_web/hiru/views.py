363a4955ad0c18caf22f53536bdce04886e21f8f
Na-sanghwa <8008165@gmail.com>
2022-05-21 23:01:06 +0900

    질문 상세 uri, 없는 페이지 조회시 ERROR 404 출력

--- f_web/hiru/views.py
+++ f_web/hiru/views.py
@@ -9,7 +9,7 @@ def index(request):
     context={'question_list': question_list}
     return render(request,'hiru/question_list.html',context)##템플릿에 적용해 html로 반환
 
-def detail(request, question_id): #uri 매핑 규칙에 의해 실행
+def detail(request, question_id): ##uri 매핑 규칙에 의해 실행
     question = get_object_or_404(Question, pk=question_id) # 없는 페이지 조회시 404 출력
     context = {'question': question}
     return render(request, 'hiru/question_detail.html', context)
