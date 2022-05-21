46a5c5c1432efb37e228e9756fed360de16ac877
Na-sanghwa <8008165@gmail.com>
2022-05-21 22:54:17 +0900

    uri 별칭 매핑

--- f_web/hiru/urls.py
+++ f_web/hiru/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name='hiru' #네임스페이스
+
 urlpatterns = [
     path('', views.index, name='index'),#별칭 사용
     path('<int:question_id>/', views.detail, name='detail'), #question_id int 매핑, 뷰 호출, 별칭 매핑
