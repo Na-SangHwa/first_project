8d92698a2f4e62e680833543cff2800873f0945f
Na-sanghwa <8008165@gmail.com>
2022-05-22 23:56:52 +0900

    답변 생성 및 읽어오기 함수 등록

--- f_web/hiru/views.py
+++ f_web/hiru/views.py
@@ -1,7 +1,8 @@
 
 
 # Create your views here.
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from .models import Question
 
 def index(request):
@@ -13,3 +14,8 @@ def detail(request, question_id): ##uri 매핑 규칙에 의해 실행
     question = get_object_or_404(Question, pk=question_id) # 없는 페이지 조회시 404 출력
     context = {'question': question}
     return render(request, 'hiru/question_detail.html', context)
+
+def answer_create(request, question_id): # 답변 생성 및 읽어오기
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('hiru:detail', question_id=question_id)
