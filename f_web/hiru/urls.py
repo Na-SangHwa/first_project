1b49645af80b864cf5bded0ed11fe3b48e4e9929
Na-sanghwa <8008165@gmail.com>
2022-05-22 23:56:18 +0900

    answer_create 호출uri 매핑

--- f_web/hiru/urls.py
+++ f_web/hiru/urls.py
@@ -6,4 +6,6 @@ app_name='hiru' #네임스페이스
 urlpatterns = [
     path('', views.index, name='index'),#별칭 사용
     path('<int:question_id>/', views.detail, name='detail'), #question_id int 매핑, 뷰 호출, 별칭 매핑
+    path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'), #answer/create/int 호출시 views.answer_create 호출
+
 ]
