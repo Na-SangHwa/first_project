6eea0c60b1f65a524b24c5e4fa4967890fdf0e96
Na-sanghwa <8008165@gmail.com>
2022-06-04 23:45:22 +0900

    저장하기 버튼 클릭 시 처리 방법

--- f_web/hiru/views.py
+++ f_web/hiru/views.py
@@ -4,6 +4,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Question
+from .forms import QuestionForm
 
 def index(request):
     question_list=Question.objects.order_by('-create_date') ##질문 목록 데이터 역순으로 정렬
@@ -19,3 +20,16 @@ def answer_create(request, question_id): # 답변 생성 및 읽어오기
     question = get_object_or_404(Question, pk=question_id)
     question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
     return redirect('hiru:detail', question_id=question_id)
+
+def question_create(request): #저장하기 요청 처리 방법
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('hiru:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'hiru/question_form.html', context)
